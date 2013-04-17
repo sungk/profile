2c4172981cecd1e74ef8d00659f4272785ec3463
sungkim <seoulcityrocker@gmail.com>
2013-04-17 19:58:58 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sungkim::Application.config.secret_token = '26382e6a3904fa3f41b53f71a4d17568b3960305b2730ff38abae176c71cefd085ba81e65caf117d40862850577971bd9b124bf3953559a46ddddd6666d13de4'
